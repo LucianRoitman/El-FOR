edfadfb766f859053886bfad448fbd381ba29e26
LucianRoitman <49261770@est.ort.edu.ar>
2025-06-19 09:54:52 -0300

    for

--- Assets/RepasoFor.cs
+++ Assets/RepasoFor.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public int numero1;
+    public int numero2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(i);
+        }
+
+        for (int i = 3; i <= 30; i += 3)
+        {
+            Debug.Log(i);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
